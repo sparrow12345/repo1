bb64c88f80bb5cc74111afe74353931e28dc0b5c
sparrow12345 <m.naser@innopolis.university>
2023-04-04 13:09:32 +0300

    f5

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,7 +13,7 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-	},
+	}, 
 	paths: {
 		base: dev ? '' : '/HW_5',
 	}
